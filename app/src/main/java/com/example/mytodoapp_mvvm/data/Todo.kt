ac6f388d06b3463d2bcdde8491b23a6707d94aec
nishant <nishantpriya.scorpion@gmail.com>
2026-01-03 21:55:00 +0530

    created Dao

--- app/src/main/java/com/example/mytodoapp_mvvm/data/Todo.kt
+++ app/src/main/java/com/example/mytodoapp_mvvm/data/Todo.kt
@@ -1,3 +1,14 @@
 package com.example.mytodoapp_mvvm.data
 
-data class Todo()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class Todo(
+    val title:String,
+    val description: String?,
+
+    val isDone: Boolean,
+ @PrimaryKey   val id: Int? = null
+)
