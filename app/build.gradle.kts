5545daf14dc37706b1dd3f574778111b5911940c
nishant <nishantpriya.scorpion@gmail.com>
2026-01-03 20:43:34 +0530

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "com.example.mytodoapp_mvvm"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.mytodoapp_mvvm"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+dependencies {
+
+    // ---------------- Core Android ----------------
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("androidx.activity:activity-compose:1.9.2")
+
+    // ---------------- Jetpack Compose ----------------
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.material:material:1.5.4")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
+    implementation(libs.androidx.material3.android)
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
+
+    // ---------------- Lifecycle / ViewModel ----------------
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
+
+    // ---------------- Navigation (Compose) ----------------
+    implementation("androidx.navigation:navigation-compose:2.8.3")
+
+    // ---------------- Dependency Injection (Hilt) ----------------
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    // ---------------- Room Database ----------------
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+
+    // ---------------- Core Library Desugaring ----------------
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.3")
+
+    // ---------------- Testing ----------------
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+}
---
+++ app/src/main/java/com/example/mytodoapp_mvvm/data/Todo.kt
@@ -0,0 +1,3 @@
+package com.example.mytodoapp_mvvm.data
+
+data class Todo()
